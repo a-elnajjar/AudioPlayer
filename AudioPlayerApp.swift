b2b58d54877155bdcbf23f9f37782aba398abf61
a-elnajjar <code@mail.com>
2023-04-10 17:16:07 -0400

    audio player

---
+++ AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by Abdalla Elnajjar on 2023-04-10.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
